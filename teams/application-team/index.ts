cd08122deaec75f7c8d4e9b79409ac693ebb1486
EC2 Default User <ec2-user@ip-172-31-8-118.us-west-2.compute.internal>
2022-08-18 21:02:26 +0000

    Fixed application team

--- teams/application-team/index.ts
+++ teams/application-team/index.ts
@@ -5,7 +5,7 @@ export class TeamApplication extends ApplicationTeam {
     constructor(name: string, accountID: string) {
         super({
             name: name,
-            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:user/platform`)]
+            users: [new ArnPrincipal(`arn:aws:iam::${accountID}:user/application`)]
         });
     }
 }
