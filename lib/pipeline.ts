29e11bf7aaa4c73eb4f79da02ca701c69d077ed6
Shardul Vaidya <vshardul@amazon.com>
2022-08-29 12:44:48 -0400

    Multi-deployment argoCD stuff

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,64 +1,64 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 
 export default class PipelineConstruct extends Construct {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id)
-        
+
         const account = props?.env?.account!;
         const region = props?.env?.region!;
-        
+
         const blueprint = blueprints.EksBlueprint.builder()
             .account(account)
             .region(region)
             .addOns(new blueprints.ClusterAutoScalerAddOn)
             .teams();
-            
+
         const repoUrl = 'https://github.com/aws-samples/eks-blueprints-workloads.git'
 
-        const bootstrapRepo : blueprints.ApplicationRepository = {
+        const bootstrapRepo: blueprints.ApplicationRepository = {
             repoUrl,
             targetRevision: 'workshop',
         }
-        
+
         const devBootstrapArgo = new blueprints.ArgoCDAddOn({
-        bootstrapRepo: {
-            ...bootstrapRepo,
-            path: 'envs/dev'
-        },
+            bootstrapRepo: {
+                ...bootstrapRepo,
+                path: 'envs/dev'
+            },
         });
-        
+
         const testBootstrapArgo = new blueprints.ArgoCDAddOn({
-        bootstrapRepo: {
-            ...bootstrapRepo,
-            path: 'envs/test'
-        },
+            bootstrapRepo: {
+                ...bootstrapRepo,
+                path: 'envs/test'
+            },
         });
-        
+
         const prodBootstrapArgo = new blueprints.ArgoCDAddOn({
-        bootstrapRepo: {
-            ...bootstrapRepo,
-            path: 'envs/prod'
-        },
+            bootstrapRepo: {
+                ...bootstrapRepo,
+                path: 'envs/prod'
+            },
         });
-            
+
         blueprints.CodePipelineStack.builder()
             .name('eks-blueprints-workshop-pipeline')
             .owner('5herlocked')
             .repository({
-                    repoUrl: 'turbo-giggle',
-                    credentialsSecretName: 'github-token',
-                    targetRevision: 'main'
+                repoUrl: 'turbo-giggle',
+                credentialsSecretName: 'github-token',
+                targetRevision: 'main'
             })
             .wave({
                 id: "envs",
                 stages: [
-                    { id: 'dev', stackBuilder: blueprint.clone('us-east-1').addOns(devBootstrapArgo) },
-                    { id: 'test', stackBuilder: blueprint.clone('us-west-2').addOns(testBootstrapArgo) },
-                    { id: 'prod', stackBuilder: blueprint.clone('us-east-2').addOns(prodBootstrapArgo) },
+                    {id: 'dev', stackBuilder: blueprint.clone('us-east-1').addOns(devBootstrapArgo)},
+                    {id: 'test', stackBuilder: blueprint.clone('us-west-2').addOns(testBootstrapArgo)},
+                    {id: 'prod', stackBuilder: blueprint.clone('us-east-2').addOns(prodBootstrapArgo)},
                 ]
             })
-            .build(scope, id+'-stack', props);
+            .build(scope, id + '-stack', props);
     }
 }
