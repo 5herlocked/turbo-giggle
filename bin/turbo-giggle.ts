5556dcdd7e350d6575dd5ecce1ea0d99260d83b5
Shardul Vaidya <vshardul@amazon.com>
2022-08-30 12:07:17 -0400

    Added in workloads

--- bin/turbo-giggle.ts
+++ bin/turbo-giggle.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import ClusterConstruct from '../lib/eks-blueprint';
-import PipelineConstruct from '../lib/pipeline';
+import PipelineConstruct from '../lib/cluster-pipeline';
+import { WorkloadBuilder } from '../lib/workload-builder';
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT!;
@@ -9,4 +10,6 @@ const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region }
  
 const eksCluster = new ClusterConstruct(app, 'cluster', { env });
+const consumerBuilder = new WorkloadBuilder(app, 'ingester-builder', '5herlocked', 'main', 'video-consumer-knative-microservice', { env });
+const ingesterBuilder = new WorkloadBuilder(app, 'consumer-builder', '5herlocked', 'main', 'kinesis-knative-ingest', { env });
 const gitopsPipeline = new PipelineConstruct(app, 'pipeline', { env });
---
+++ lib/workload-builder.ts
@@ -0,0 +1,73 @@
+import * as cdk from 'aws-cdk-lib';
+import { BuildEnvironmentVariableType, Project, Source } from 'aws-cdk-lib/aws-codebuild';
+import { Construct } from 'constructs';
+
+export class WorkloadBuilder extends cdk.Stack {
+  constructor(scope: Construct, id: string, owner: string, branch: string, repoName: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+
+    const repo = Source.gitHub({
+      owner: owner,
+      repo: repoName,
+      branchOrRef: branch,
+      webhook: true,
+    });
+
+    const buildProject = new Project(this, 'CodeBuildProject', {
+      projectName: repoName,
+      source: repo,
+      environmentVariables: {
+        'AWS_DEFAULT_REGION': {
+          type: BuildEnvironmentVariableType.PLAINTEXT,
+          value: region
+        },
+        'AWS_ACCOUNT_ID': {
+          type: BuildEnvironmentVariableType.PLAINTEXT,
+          value: account
+        },
+        'IMAGE_TAG': {
+          type: BuildEnvironmentVariableType.PLAINTEXT,
+          value: 'latest'
+        },
+        'IMAGE_REPO_NAME': {
+          type: BuildEnvironmentVariableType.PLAINTEXT,
+          value: repoName,
+        }
+      }
+    });
+
+    // const input = CodePipelineSource.gitHub(githubURL, branch);
+
+    // const pipelineProject = new PipelineProject(this, 'Project', {
+    //   projectName: 'KinesisIngestBuilder',
+    //   environment: {
+    //     buildImage: LinuxBuildImage.STANDARD_6_0,
+    //     privileged: true,
+    //     computeType: ComputeType.SMALL,
+    //     environmentVariables: {
+    //       'AWS_DEFAULT_REGION': {
+    //         type: BuildEnvironmentVariableType.PLAINTEXT,
+    //         value: region
+    //       },
+    //       'AWS_ACCOUNT_ID': {
+    //         type: BuildEnvironmentVariableType.PLAINTEXT,
+    //         value: account
+    //       },
+    //       'IMAGE_TAG': {
+    //         type: BuildEnvironmentVariableType.PLAINTEXT,
+    //         value: 'latest'
+    //       },
+    //       'IMAGE_REPO_NAME': {
+    //         type: BuildEnvironmentVariableType.PLAINTEXT,
+    //         value: repoName,
+    //       }
+    //     }
+    //   }
+    // });
+
+    
+  }
+}
