72461231aed2ae4e1bbfcede540225c0de23ed94
EC2 Default User <ec2-user@ip-172-31-87-79.ec2.internal>
2022-08-31 05:43:58 +0000

    Added in custom service policy for workload builder?

--- lib/workload-builder.ts
+++ lib/workload-builder.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { BuildEnvironmentVariableType, Project, Source } from 'aws-cdk-lib/aws-codebuild';
 import { Construct } from 'constructs';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
+import { Role, ServicePrincipal, PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export class WorkloadBuilder extends cdk.Stack {
   constructor(scope: Construct, id: string, owner: string, branch: string, repoName: string, props?: cdk.StackProps) {
@@ -12,7 +13,30 @@ export class WorkloadBuilder extends cdk.Stack {
     
     const ecr_repo = new Repository(this, 'ECR-Repository', {
       repositoryName: repoName,
-    })
+    });
+    
+    const custom_service_policy = new PolicyDocument({
+      statements: [new PolicyStatement({
+        actions: [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ],
+        resources: ['*'],
+      })],
+    });
+    
+    const service_role = new Role(this, 'Service-Role', {
+      roleName: repoName + '-code-build-service-role',
+      assumedBy: new ServicePrincipal('codebuild.amazonaws.com'),
+      description: 'Service Role to be assumed by the CodeBuild to publish to ECR',
+      inlinePolicies: {
+        'Custom-Policy': custom_service_policy,
+      }
+    });
 
     const repo = Source.gitHub({
       owner: owner,
@@ -41,38 +65,8 @@ export class WorkloadBuilder extends cdk.Stack {
           type: BuildEnvironmentVariableType.PLAINTEXT,
           value: repoName,
         }
-      }
+      },
+      role: service_role,
     });
-
-    // const input = CodePipelineSource.gitHub(githubURL, branch);
-
-    // const pipelineProject = new PipelineProject(this, 'Project', {
-    //   projectName: 'KinesisIngestBuilder',
-    //   environment: {
-    //     buildImage: LinuxBuildImage.STANDARD_6_0,
-    //     privileged: true,
-    //     computeType: ComputeType.SMALL,
-    //     environmentVariables: {
-    //       'AWS_DEFAULT_REGION': {
-    //         type: BuildEnvironmentVariableType.PLAINTEXT,
-    //         value: region
-    //       },
-    //       'AWS_ACCOUNT_ID': {
-    //         type: BuildEnvironmentVariableType.PLAINTEXT,
-    //         value: account
-    //       },
-    //       'IMAGE_TAG': {
-    //         type: BuildEnvironmentVariableType.PLAINTEXT,
-    //         value: 'latest'
-    //       },
-    //       'IMAGE_REPO_NAME': {
-    //         type: BuildEnvironmentVariableType.PLAINTEXT,
-    //         value: repoName,
-    //       }
-    //     }
-    //   }
-    // });
-
-    
   }
 }
